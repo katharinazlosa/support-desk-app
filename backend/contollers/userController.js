0956dd6daec4f97ea7e2869ebbe63638848735b3
Katharina Zlosa <63505535+khZlo@users.noreply.github.com>
2022-09-15 22:06:30 +0200

    connected to db

--- backend/contollers/userController.js
+++ backend/contollers/userController.js
@@ -1,17 +1,27 @@
+const asyncHandler = require('express-async-handler')
+
 // @desc   register a new user
 // @route  /api/users
 // @access Public 
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
+    const {name, email, password } = req.body
+
+    //validation
+    if (!name || !email || !password) {
+        return res.status(400)
+        throw new Error('Please include all fields')
+    }
+
     res.send('Register route')
-}
+})
 
-// @desc   register a new user
-// @route  /api/users
+// @desc   login a new user
+// @route  /api/users/login
 // @access Public 
 
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
     res.send('Login route')
-}
+})
 
 module.exports = {
     registerUser,
